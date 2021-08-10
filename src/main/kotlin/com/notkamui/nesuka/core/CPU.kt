8a9bcbea080e4a4fb3a47fc1ea35de47e70f9559
notKamui <teillard.jimmy@outlook.fr>
2021-08-10 22:55:57 +0200

    Added CPU class representation

---
+++ src/main/kotlin/com/notkamui/nesuka/core/CPU.kt
@@ -0,0 +1,11 @@
+package com.notkamui.nesuka.core
+
+class CPU {
+    var registerA: UByte = 0u
+    var status: UByte = 0u
+    var programCounter = 0u
+
+    fun interpret(program: List<UByte>) {
+        TODO()
+    }
+}
