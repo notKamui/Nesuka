e4dbd60e2680ec0bcfec9f55528c94dd5d2f48c2
notKamui <teillard.jimmy@outlook.fr>
2021-08-11 23:53:06 +0200

    Refactor

--- src/main/kotlin/com/notkamui/nesuka/utils/IntUtils.kt
+++ src/main/kotlin/com/notkamui/nesuka/utils/IntUtils.kt
@@ -6,6 +6,6 @@ val Int.u8: UByte
 val Int.u16: UShort
     get() = toUShort()
 
-infix fun UShort.shl(bitshift: Int) = this.toInt() shl bitshift
+infix fun UShort.shl(bitshift: Int): Int = this.toInt() shl bitshift
 
-infix fun UShort.shr(bitshift: Int) = this.toInt() shr bitshift
+infix fun UShort.shr(bitshift: Int): Int = this.toInt() shr bitshift
