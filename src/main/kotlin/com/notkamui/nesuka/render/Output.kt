b6eb50052630cdc013963c9f195ba1a588b07c40
notKamui <teillard.jimmy@outlook.fr>
2021-08-14 14:18:09 +0200

    Implemented screen state.

--- src/main/kotlin/com/notkamui/nesuka/render/Output.kt
+++ src/main/kotlin/com/notkamui/nesuka/render/Output.kt
@@ -1,5 +1,7 @@
 package com.notkamui.nesuka.render
 
+import com.notkamui.nesuka.core.CPU
+import com.notkamui.nesuka.utils.u16
 import com.notkamui.nesuka.utils.u8
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.paint.Color
@@ -10,16 +12,46 @@ private fun GraphicsContext.drawPixel(x: Int, y: Int, color: Color) {
 }
 
 class Bitmap(val width: Int, val height: Int) {
-    private val bitmap =
-        Array(32) { Array(32) { Triple(0.u8, 0.u8, 0.u8) } }
+    private val bitmap = Array(width * height * 3) { 0.u8 }
 
-    operator fun get(x: Int, y: Int) = bitmap[y][x]
+    operator fun get(frameIndex: Int) =
+        bitmap[frameIndex]
 
-    operator fun set(x: Int, y: Int, rgb: Triple<UByte, UByte, UByte>) {
-        bitmap[y][x] = rgb
+    operator fun get(x: Int, y: Int): Color {
+        val frameIndex = (y * width + x) * 3
+        return Color.rgb(
+            bitmap[frameIndex].toInt(),
+            bitmap[frameIndex + 1].toInt(),
+            bitmap[frameIndex + 2].toInt()
+        )
+    }
+
+    operator fun set(frameIndex: Int, byte: UByte) {
+        bitmap[frameIndex] = byte
     }
 
     operator fun set(x: Int, y: Int, color: Color) {
-        bitmap[y][x] = color.rgb()
+        val frameIndex = (y * width + x) * 3
+        val (r, g, b) = color.rgb()
+        bitmap[frameIndex] = r
+        bitmap[frameIndex + 1] = g
+        bitmap[frameIndex + 2] = b
     }
+}
+
+fun Bitmap.readScreenState(cpu: CPU): Boolean {
+    var frameIndex = 0
+    var update = false
+    for (i in 0x0200..0x600) {
+        val colorIndex = cpu.memRead(i.u16)
+        val (r, g, b) = colorIndex.colorFromByte().rgb()
+        if (this[frameIndex] != r || this[frameIndex + 1] != g || this[frameIndex + 2] != b) {
+            this[frameIndex] = r
+            this[frameIndex + 1] = g
+            this[frameIndex + 2] = b
+            update = true
+        }
+        frameIndex += 3
+    }
+    return update
 }
